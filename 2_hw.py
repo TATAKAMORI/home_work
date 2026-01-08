9d6a46e7aa1a7bde640d0b3bff010071242436f5
TATAKAMORI <bukinmis@gmail.com>
2026-01-08 22:31:48 +0300

    Add files via upload
    Home work 2

---
+++ 2_hw.py
@@ -0,0 +1,28 @@
+def task_1():
+    a: int = 1
+    b: float = 2.1
+    c: str = 'кек'
+    d: list = [1, 2, 3, 'meme']
+    e: bool = True
+
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+
+print(task_1())
+
+
+
+def task_2():
+    a = [1, 2, 3, 5, 8, 13, 21] #Последовательность Фибоначчи
+    return a[0:3]
+
+print(task_2())
+
+
+def task_3(a):
+    return a**2
+
+print(task_3(5))
